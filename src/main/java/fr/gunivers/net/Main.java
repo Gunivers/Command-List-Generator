fe578ce813bd8f80ad88ae2ebd5a2f1a9bcc3e1d
MrBretzel <axelolo68@gmail.com>
2018-01-17 00:12:28 +0100

    A test

---
+++ src/main/java/fr/gunivers/net/Main.java
@@ -0,0 +1,18 @@
+package fr.gunivers.net;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+
+        //ici sa start
+
+    }
+}
