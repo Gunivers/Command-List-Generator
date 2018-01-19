09f8866203fa65a9a2e71d4e9441e37aa468c695
KubbyDev <gbrljorge@gmail.com>
2018-01-19 19:24:32 +0100

    Dites moi si vous voyez mon push

--- CLG/src/Main.java
+++ CLG/src/Main.java
@@ -6,9 +6,17 @@ public class Main {
 	//Cette méthode sera ensuite inutile, puisque le bouton "Generate" appellera directement la méthode generate()
 	public static void main(String[] args) {
 	
-		String entree = "#Interp:0,100,1.8,0,0#";
-		disp(generateInterp(entree, 10));
+		String entree = "#InterpScore:0,100,1.8,0,GNE#";
+		//disp(generateInterp(entree, 1));
 		//Modif test test
+		
+
+		System.out.print("\n\n");
+		
+		disp(readTag(entree));
+		
+		
+		
 	}
 	
 	public static void disp(double[] d) {
@@ -25,11 +33,60 @@ public class Main {
 		
 	}
 	
-	
-	
-	
-	
-	
+	public static String readName(String balise) {
+		    
+		String name = "";
+		int i = 1;    
+		
+		do {
+			name += balise.charAt(i);
+			i++;  
+		} while(balise.charAt(i) != ':');
+		    
+		return name;
+	}
+		  
+	public static String[] readTag(String balise) {
+		    
+		int beginIndex = 0;
+		int nbreArgs = 0;
+		    
+	    switch(readName(balise)) {  
+	    case "Interp":
+	        beginIndex = 8;
+	        nbreArgs = 5;
+	        break;
+	    case "InterpScore":
+	        beginIndex = 13;
+	        nbreArgs = 5;
+	        break;
+	    default:;
+	    }
+	    
+	    String[] args = new String[nbreArgs];
+	    
+	    int i = beginIndex;
+	    int numeroArg = 0;
+	    String argument = "";
+	    
+	    do {
+			
+			if(balise.charAt(i) == ',') {
+				args[numeroArg] = argument;
+				argument = "";
+				numeroArg++;
+			}
+			else
+				argument += balise.charAt(i);
+			
+			i++;
+			
+		} while(balise.charAt(i) != '#');
+		//On lit le dernier (qui n'est pas détecté comme les autres par une virgule)
+		args[nbreArgs-1] = argument;
+	    
+	    return args;
+	}
 	
 	public static double linearInterp(double debut, double fin, double alpha) {
 		return fin*alpha + debut;
@@ -37,7 +94,12 @@ public class Main {
 	
 	public static double interp(double debut, double fin, int nbreCommandes, int numeroCommande, double puissance, boolean invert) {
 		
-		double alpha = (double) numeroCommande/ (double) (nbreCommandes-1);	
+		double alpha;
+		if(nbreCommandes > 1)
+			alpha = (double) numeroCommande/ (double) (nbreCommandes-1);	
+		else 
+			alpha = 0.5;
+		
 		if(invert)
 			alpha = 1-alpha;
 		
@@ -51,47 +113,27 @@ public class Main {
 	
 	public static String[] generateInterp(String balise, int nbreCommandes) {
 	
-		double[] infos = readInterp(balise);
+		String[] infosS = readTag(balise);
+		double[] infos = new double[5];
+		//index 0: nombre de départ
+		//index 1: nombre de fin
+		//index 2: puissance (courbure de la montée)
+		//index 3: invert (0 ou 1)
+		//index 4: nombre de décimales
 		
-		double debut = infos[0];
-		double fin = infos[1];
-		double puissance = infos[2];
-		boolean invert = infos[3] == 1;
-		int decimalsNumber = (int) infos[4];
+		for(int i = 0; i <= 4; i++) {
+			infos[i] = Double.valueOf(infosS[i]);
+		}
 		
 		String[] ret = new String[nbreCommandes];
 		
 		for(int i = 0; i < nbreCommandes; i++)
-			if(decimalsNumber == 0)
-				ret[i] = String.valueOf((int) Math.round(interp(debut, fin, nbreCommandes, i, puissance, invert)));
+			if(infos[4] == 0)
+				ret[i] = String.valueOf((int) Math.round(interp(infos[0], infos[1], nbreCommandes, i, infos[2], infos[3] == 1)));
 			else
-				ret[i] = String.valueOf(round(interp(debut, fin, nbreCommandes, i, puissance, invert), decimalsNumber));
+				ret[i] = String.valueOf(round(interp(infos[0], infos[1], nbreCommandes, i, infos[2], infos[3] == 1), (int) infos[4]));
 				
 		return ret;
-	}
-	
-	public static double[] readInterp(String balise) {
-		
-		double[] ret = new double[5];
-		int index = 0;
-		String valeur = "";
-		
-		//On commence apres "#Interp:"
-		for(int i = 8; i < balise.length()-1; i++) {
-			
-			if(balise.charAt(i) == ',') {
-				ret[index] = Double.valueOf(valeur);
-				valeur = "";
-				index++;
-			}
-			else
-				valeur += balise.charAt(i);
-			
-		}
-		//On lit le dernier (qui n'est pas détecté comme les autres par une virgule)
-		ret[4] = Double.valueOf(valeur);
-		
-		return ret;		
 	}
 	
 	public static double round(double number, int decimalsNumber) {
@@ -100,14 +142,6 @@ public class Main {
 		number *= power;
 		return ((double) Math.round(number)) / power;
 	}
-	
-	
-	
-	
-	
-	
-	
-	
 	
 	public static ArrayList<String> generate(String entree) {
 		
