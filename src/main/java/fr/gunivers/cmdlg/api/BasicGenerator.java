8c536fca69d1acc952e9a447776abfa8f326b2d2
A:Z <justin.carel@free.fr>
2018-01-24 21:48:15 +0100

    JavaDoc BasicGenerator

--- src/main/java/fr/gunivers/cmdlg/api/BasicGenerator.java
+++ src/main/java/fr/gunivers/cmdlg/api/BasicGenerator.java
@@ -6,15 +6,32 @@ public abstract class BasicGenerator {
 
     private String[] command;
 
+    /**Constructor
+     * 
+     * @param command to generate
+     */
     public BasicGenerator(String... command) {
         this.command = command;
     }
 
+    /**Generator
+     * 
+     * @return the generated command
+     */
     public abstract Iterable<? extends String> generate();
 
+    
+    /**Get the registered command
+     * 
+     * @return command
+     */
     public String[] getCommand() {
         return command;
     }
 
+    /**Get the registered type
+     * 
+     * @return type
+     */
     public abstract GeneratorType getType();
 }
