8eec4c6bdb4886b2bbd370750af6c503c4036136
A:Z <justin.carel@free.fr>
2018-01-25 23:31:23 +0100

    Add FuelGenerator & FlammableGenerator

---
+++ src/main/java/fr/gunivers/cmdlg/generators/FlammableGenerator.java
@@ -0,0 +1,48 @@
+package fr.gunivers.cmdlg.generators;
+
+import java.util.ArrayList;
+
+import fr.gunivers.cmdlg.util.GeneratorType;
+import fr.gunivers.cmdlg.util.Material;
+
+public class FlammableGenerator extends MaterialGenerator {
+	
+	private GeneratorType type = GeneratorType.FLAMMABLE;
+	
+	/**
+     * Generate Blocks Material
+     *
+     * @return ArrayList<String> commands
+     */
+    @Override
+    public Iterable<? extends String> generate() {
+        ArrayList<String> commands = new ArrayList<>();
+
+        for (Material material : Material.values()) {
+        	if (material.isFlammable()) {
+        		StringBuilder temp = new StringBuilder();
+        		
+        		for (int e = 0; e < getCommand().length - 1; e++) {
+        			temp.append(getCommand()[e]).append(material.name());
+        			
+        			if (e == getCommand().length - 2) temp.append(getCommand()[e + 1]);
+	           }
+        		
+        		commands.add(temp.toString());
+        	}
+        }
+        return commands;
+    }
+
+    /**
+     * Get the Materials to be generated
+     *
+     * @return ArrayList<Material> materialList
+     */
+    public Iterable<? extends Material> getMaterialList() {
+    	ArrayList<Material> values = new ArrayList<Material>();
+    	for (Material matos : Material.values()) if (matos.isFlammable()) values.add(matos);
+    	
+        return values;
+    }
+}
