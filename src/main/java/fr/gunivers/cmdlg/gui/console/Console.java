c1391b07424b2dd171e7d95fe7c5c190edec3736
MrBretzel <axelolo68@gmail.com>
2018-02-02 22:06:29 +0100

    Comment and fix Console

--- src/main/java/fr/gunivers/cmdlg/gui/console/Console.java
+++ src/main/java/fr/gunivers/cmdlg/gui/console/Console.java
@@ -27,25 +27,32 @@ public class Console {
      * Start console.
      */
     public static void start() {
+        //if mainFrame is visible to prevent bug
         if (!mainFrame.isVisible()) {
 
+            //set the new print stream.
             System.setOut(new PrintStream(new ConsoleOutputStream(), true));
             System.setErr(new PrintStream(new ConsoleErrOutputStream(), true));
 
+            //init the tabbed pane
             JTabbedPane tabbedPane = new JTabbedPane();
 
+            //init all tab
             initTab(allTextPane);
             initTab(infoTextPane);
             initTab(debugTextPane);
 
+            //adding tab to the pane
             tabbedPane.addTab("Info Log", new JTable().add(new JScrollPane(infoTextPane)));
             tabbedPane.addTab("Debug Log", new JTable().add(new JScrollPane(debugTextPane)));
             tabbedPane.addTab("All Log", new JTable().add(new JScrollPane(allTextPane)));
 
+            //#FixJava (tab has not the good pane)
             tabbedPane.setComponentAt(0, new JScrollPane(infoTextPane));
             tabbedPane.setComponentAt(1, new JScrollPane(debugTextPane));
             tabbedPane.setComponentAt(2, new JScrollPane(allTextPane));
 
+            //#FixJava 2 (ScrollPane not refreshing on resize)
             tabbedPane.addComponentListener(new ComponentAdapter() {
                 @Override
                 public void componentResized(ComponentEvent e) {
@@ -58,10 +65,15 @@ public class Console {
                 }
             });
 
-            tabbedPane.setSize(500, 400);
-            mainFrame.add(tabbedPane);
+            //set the dimension of the windows
+            mainFrame.setPreferredSize(new Dimension(750, 400));
+            //add tab pane
+            mainFrame.add(tabbedPane, BorderLayout.CENTER);
+            //sync close with the main app
             mainFrame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+            //pack the main for validate all option
             mainFrame.pack();
+            //set main frame visible
             mainFrame.setVisible(true);
         }
     }
@@ -79,11 +91,15 @@ public class Console {
      * @param color
      */
     private static void addCmd(String text, Color color) {
+        //get the StyledDoc
         StyledDocument doc = allTextPane.getStyledDocument();
+        //add a new Style for StyledDoc
         Style style = allTextPane.addStyle("Color Style", null);
+        //set the new Style
         StyleConstants.setForeground(style, color);
 
         try {
+            //insert string to TextPane
             doc.insertString(doc.getLength(), text, style);
         }
         catch (BadLocationException e) {
@@ -97,8 +113,11 @@ public class Console {
      * @param color
      */
     private static void addText(JTextPane pane, String text, Color color) {
+        //get the StyledDoc
         StyledDocument doc = pane.getStyledDocument();
+        //add a new Style for StyledDoc
         Style style = pane.addStyle("Color Style", null);
+        //set the new Style
         StyleConstants.setForeground(style, color);
 
         if (!text.endsWith("\n"))
@@ -106,6 +125,7 @@ public class Console {
 
 
         try {
+            //insert string to TextPane
             doc.insertString(doc.getLength(), text, style);
         }
         catch (BadLocationException e) {
@@ -113,6 +133,10 @@ public class Console {
         }
     }
 
+    /**
+     * Log info on the console
+     * @param text
+     */
     public static void logInfo(String text) {
         Date date = new Date();
         String d = new SimpleDateFormat("HH:mm:ss").format(date);
@@ -120,13 +144,17 @@ public class Console {
         addText(allTextPane, d + " [INFO]: " + text, Color.ORANGE);
     }
 
+    /**
+     * Log debug on the console
+     * @param text
+     */
     public static void logDebug(String text) {
-            StackTraceElement[] stackTrace = Thread.currentThread().getStackTrace();
-            StackTraceElement element = stackTrace[2];
-            String d = new SimpleDateFormat("HH:mm:ss").format(new Date());
-            String t = "<" + element.getClassName() + ":" + element.getLineNumber() + ":" + element.getMethodName() + "> " + d;
-            addText(debugTextPane, t + " [DEBUG]: " + text, Color.MAGENTA);
-            addText(allTextPane, t + " [DEBUG]: " + text, Color.MAGENTA);
+        StackTraceElement[] stackTrace = Thread.currentThread().getStackTrace();
+        StackTraceElement element = stackTrace[2];
+        String d = new SimpleDateFormat("HH:mm:ss").format(new Date());
+        String t = "<" + element.getClassName() + ":" + element.getLineNumber() + ":" + element.getMethodName() + "> " + d;
+        addText(debugTextPane, t + " [DEBUG]: " + text, Color.MAGENTA);
+        addText(allTextPane, t + " [DEBUG]: " + text, Color.MAGENTA);
     }
 
     /**
@@ -149,6 +177,10 @@ public class Console {
         }
     }
 
+    /**
+     * init tab for custom display
+     * @param textPane
+     */
     private static void initTab(JTextPane textPane) {
         textPane.setFont(new Font(Font.SANS_SERIF, Font.PLAIN, 14));
         textPane.setEditable(false);
