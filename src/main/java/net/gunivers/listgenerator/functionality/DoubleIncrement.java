1dacd8ee84af6294c087c8b777bbafc07edb2774
A:Z <justin.carel@free.fr>
2018-03-23 13:01:11 +0100

    Add getIncrement to DoubleIncrement

--- src/main/java/net/gunivers/listgenerator/functionality/DoubleIncrement.java
+++ src/main/java/net/gunivers/listgenerator/functionality/DoubleIncrement.java
@@ -1,11 +1,24 @@
 package net.gunivers.listgenerator.functionality;
 
+import java.lang.reflect.Method;
 import java.util.ArrayList;
 
+/**
+ * 
+ * @author AZ
+ *
+ */
 public class DoubleIncrement {
 	
+	public static Method getIncrement() {
+			try { return DoubleIncrement.class.getMethod("increment", Double.class, Double.class, Integer.class);} 
+			
+			catch (NoSuchMethodException | SecurityException e) {e.printStackTrace();}
+			
+			return null;
+	}
 	
-	public static ArrayList<String> incremente(Double init, Double increment, Integer nbLoop) {
+	public static ArrayList<String> increment(Double init, Double increment, Integer nbLoop) {
 		ArrayList<String> save = new ArrayList<String>();
 		save.add(Double.toString(init));
 		
