b7de53b61619a2a195ae7b30d8b12ae5c74f73d0
A:Z <justin.carel@free.fr>
2018-03-31 18:47:26 +0200

    Moved BurnableGenerator

---
+++ src/main/java/net/gunivers/listgenerator/functionality/material/BurnableGenerator.java
@@ -0,0 +1,38 @@
+package net.gunivers.listgenerator.functionality.material;
+
+import net.gunivers.cmdlg.util.GeneratorType;
+import net.gunivers.cmdlg.util.Material;
+import net.gunivers.listgenerator.Call;
+import net.gunivers.listgenerator.functionality.Functionality;
+
+import java.util.ArrayList;
+
+public class BurnableGenerator extends Functionality
+{
+
+    /**
+     * Generate Burnable Material
+     *
+     * @return ArrayList<String> commands
+     */
+	@Call
+    public ArrayList<String> generate()
+    {
+        ArrayList<String> commands = new ArrayList<>();
+
+        for (Material material : Material.values())
+        {
+            if (material.isBurnable())
+            {
+                commands.add(material.name());
+            }
+        }
+        return commands;
+    }
+
+	@Override
+	public String toString() {
+		// TODO Auto-generated method stub
+		return "Burnable";
+	}
+}
