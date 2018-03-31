4d0f8aa996003c2c6d0367417276473e8190b37b
A:Z <justin.carel@free.fr>
2018-03-31 21:27:08 +0200

    Add IDItemGenerator

---
+++ src/main/java/net/gunivers/listgenerator/functionality/materialID/IDItemGenerator.java
@@ -0,0 +1,41 @@
+package net.gunivers.listgenerator.functionality.materialID;
+
+
+import net.gunivers.cmdlg.util.Material;
+import net.gunivers.listgenerator.functionality.Functionality;
+import net.gunivers.listgenerator.util.Call;
+
+import java.util.ArrayList;
+
+
+public class IDItemGenerator extends Functionality
+{
+
+    /**
+     * Generate Blocks ID
+     *
+     * @return ArrayList<String> commands
+     */
+    @SuppressWarnings("deprecation")
+	@Call
+    public ArrayList<String> generate()
+    {
+        ArrayList<String> commands = new ArrayList<>();
+
+        for (Material material : Material.values())
+        {
+
+            if (!material.isBlock())
+            {
+                commands.add(((Integer) material.getId()).toString());
+            }
+
+        }
+        return commands;
+    }
+    
+	@Override
+	public String toString() {
+		return "Item ID";
+	}
+}
