7c90cd130ee668cee32a75748bc1fc816430e54a
A:Z <justin.carel@free.fr>
2018-03-31 18:56:17 +0200

    Moved OccludingGenerator + Deleted Old Materiel

---
+++ src/main/java/net/gunivers/listgenerator/functionality/material/OccludingGenerator.java
@@ -0,0 +1,38 @@
+package net.gunivers.listgenerator.functionality.material;
+
+import net.gunivers.cmdlg.util.GeneratorType;
+import net.gunivers.cmdlg.util.Material;
+import net.gunivers.listgenerator.Call;
+import net.gunivers.listgenerator.functionality.Functionality;
+
+import java.util.ArrayList;
+
+public class OccludingGenerator extends Functionality
+{
+
+    /**
+     * Generate Occluding Material
+     *
+     * @return ArrayList<String> commands
+     */
+    @Call
+    public ArrayList<String> generate()
+    {
+        ArrayList<String> commands = new ArrayList<>();
+
+        for (Material material : Material.values())
+        {
+            if (material.isOccluding())
+            {
+                commands.add(material.name());
+            }
+        }
+        return commands;
+    }
+
+	@Override
+	public String toString() {
+		return "Occluding";
+	}
+}
+
