e01736c0eef2baa4e563a9d6713378c47f47594f
A:Z <justin.carel@free.fr>
2018-03-31 21:16:47 +0200

    Updated IDFuelGenerator

--- src/main/java/net/gunivers/listgenerator/functionality/materialID/IDFuelGenerator.java
+++ src/main/java/net/gunivers/listgenerator/functionality/materialID/IDFuelGenerator.java
@@ -1,27 +1,23 @@
 package net.gunivers.listgenerator.functionality.materialID;
 
-import net.gunivers.cmdlg.util.GeneratorType;
+
 import net.gunivers.cmdlg.util.Material;
+import net.gunivers.listgenerator.functionality.Functionality;
+import net.gunivers.listgenerator.util.Call;
 
 import java.util.ArrayList;
 
 
-public class IDFuelGenerator extends IDMaterialGenerator
+public class IDFuelGenerator extends Functionality
 {
-
-    private GeneratorType type = GeneratorType.FUEL_ID;
-
-    public IDFuelGenerator(String... commands)
-    {
-        super(commands);
-    }
 
     /**
      * Generate Fuels ID
      *
      * @return ArrayList<String> commands
      */
-    @Override
+    @SuppressWarnings("deprecation")
+	@Call
     public ArrayList<String> generate()
     {
         ArrayList<String> commands = new ArrayList<>();
@@ -31,39 +27,15 @@ public class IDFuelGenerator extends IDMaterialGenerator
 
             if (material.isFuel())
             {
-                StringBuilder temp = new StringBuilder();
-
-                for (int e = 0; e < getCommand().length - 1; e++)
-                {
-                    temp.append(getCommand()[e]).append(material.getId());
-
-                    if (e == getCommand().length - 2) temp.append(getCommand()[e + 1]);
-                }
-
-                commands.add(temp.toString());
+                commands.add(((Integer) material.getId()).toString());
             }
 
         }
         return commands;
     }
 
-    /**
-     * Get the Fuels ID to be generated
-     *
-     * @return ArrayList<Material> material ID List
-     */
-    public Iterable<? extends Integer> getIDList()
-    {
-        ArrayList<Integer> matos = new ArrayList<Integer>();
-
-        for (Material material : Material.values()) if (material.isFuel()) matos.add(material.getId());
-
-        return matos;
-    }
-
-    @Override
-    public GeneratorType getType()
-    {
-        return type;
-    }
+	@Override
+	public String toString() {
+		return "Fuel ID";
+	}
 }
