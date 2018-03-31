6992afe34caab8fc7a75be454b400d5fa45507a5
A:Z <justin.carel@free.fr>
2018-03-31 18:52:41 +0200

    Moved FuelGenerator

---
+++ src/main/java/net/gunivers/listgenerator/functionality/material/FuelGenerator.java
@@ -0,0 +1,37 @@
+package net.gunivers.listgenerator.functionality.material;
+
+import net.gunivers.cmdlg.util.GeneratorType;
+import net.gunivers.cmdlg.util.Material;
+import net.gunivers.listgenerator.Call;
+import net.gunivers.listgenerator.functionality.Functionality;
+
+import java.util.ArrayList;
+
+public class FuelGenerator extends Functionality
+{
+
+    /**
+     * Generate Fuel Material
+     *
+     * @return ArrayList<String> commands
+     */
+    @Call
+    public ArrayList<String> generate()
+    {
+        ArrayList<String> commands = new ArrayList<>();
+
+        for (Material material : Material.values())
+        {
+            if (material.isFuel())
+            {
+                commands.add(material.name());
+            }
+        }
+        return commands;
+    }
+    
+	@Override
+	public String toString() {
+		return "Fuel";
+	}
+}
