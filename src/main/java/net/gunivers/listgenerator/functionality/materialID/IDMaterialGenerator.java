57cc087d8a5d03aae5eb2a7d70c330a2fee9db48
A:Z <justin.carel@free.fr>
2018-03-31 21:19:17 +0200

    Updated IDMaterialGenerator

--- src/main/java/net/gunivers/listgenerator/functionality/materialID/IDMaterialGenerator.java
+++ src/main/java/net/gunivers/listgenerator/functionality/materialID/IDMaterialGenerator.java
@@ -1,71 +1,37 @@
 package net.gunivers.listgenerator.functionality.materialID;
 
-import net.gunivers.cmdlg.api.BasicGenerator;
-import net.gunivers.cmdlg.util.GeneratorType;
 import net.gunivers.cmdlg.util.Material;
+import net.gunivers.listgenerator.functionality.Functionality;
+import net.gunivers.listgenerator.util.Call;
 
 import java.util.ArrayList;
 
 
-public class IDMaterialGenerator extends BasicGenerator
+public class IDMaterialGenerator extends Functionality
 {
-
-    private GeneratorType type = GeneratorType.MATERIAL_ID;
-
-    public IDMaterialGenerator(String... commands)
-    {
-        super(commands);
-    }
 
     /**
      * Generate Material ID
      *
      * @return ArrayList<String> commands
      */
-    @Override
+    @SuppressWarnings("deprecation")
+	@Call
     public ArrayList<String> generate()
     {
         ArrayList<String> commands = new ArrayList<>();
 
         for (Material material : Material.values())
         {
-            StringBuilder temp = new StringBuilder();
-
-            for (int e = 0; e < getCommand().length - 1; e++)
-            {
-                temp.append(getCommand()[e]).append(material.getId());
-
-                if (e == getCommand().length - 2) temp.append(getCommand()[e + 1]);
-            }
-
-            commands.add(temp.toString());
+            commands.add(((Integer) material.getId()).toString());
         }
 
         return commands;
     }
 
-    /**
-     * Get the registered type
-     *
-     * @return GeneratorType type
-     */
-    @Override
-    public GeneratorType getType()
-    {
-        return type;
-    }
 
-    /**
-     * Get the Materials ID to be generated
-     *
-     * @return ArrayList<Material> material ID List
-     */
-    public Iterable<? extends Integer> getIDList()
-    {
-        ArrayList<Integer> matos = new ArrayList<Integer>();
-
-        for (Material material : Material.values()) matos.add(material.getId());
-
-        return matos;
-    }
+	@Override
+	public String toString() {
+		return "Material ID";
+	}
 }
