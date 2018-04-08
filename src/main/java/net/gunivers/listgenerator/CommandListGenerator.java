3b092e87f3e8c4226774817a2f41b43ce4b909ee
MrBretzel <Axelolo68@gmail.com>
2018-04-08 02:47:10 +0200

    Adding CommandListGeneratorController and java doc

--- src/main/java/net/gunivers/listgenerator/CommandListGenerator.java
+++ src/main/java/net/gunivers/listgenerator/CommandListGenerator.java
@@ -5,6 +5,7 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import net.gunivers.listgenerator.gui.CommandListGeneratorController;
 
 public class CommandListGenerator extends Application {
 
@@ -13,28 +14,44 @@ public class CommandListGenerator extends Application {
         launch(args);
     }
 
+    /**
+     * Starting of interface
+     * @param primaryStage Default stage giving by JavaFX
+     */
     @Override
     public void start(Stage primaryStage) throws Exception {
+        //Start of fxml load
         FXMLLoader loader = new FXMLLoader(getClass().getResource("/fxml/CommandListGenerator.fxml"));
 
+        //Set the controller of loader
+        loader.setController(new CommandListGeneratorController());
+
+        //Load the loader
         loader.load();
 
+        //Decorator of window
         JFXDecorator decorator = new JFXDecorator(primaryStage, loader.getRoot(), false, true, true);
 
+        //Set the tittle of window
         decorator.setText("Command List Generator");
+        primaryStage.setTitle("Command List Generator");
 
+        //Create new scene
         Scene scene = new Scene(decorator);
 
+        //Clear all CSS option
         scene.getStylesheets().clear();
+        //Add custom CSS value
         scene.getStylesheets().add(getClass().getResource("/css/Gunivers.css").toExternalForm());
 
+        //Set the dimension of window
         primaryStage.setMinWidth(640);
         primaryStage.setMinHeight(400);
 
-        primaryStage.setTitle("Command List Generator");
-
+        //Set the scene
         primaryStage.setScene(scene);
 
+        //Show the scene
         primaryStage.show();
     }
 }
---
+++ src/main/java/net/gunivers/listgenerator/gui/CommandListGeneratorController.java
@@ -0,0 +1,37 @@
+package net.gunivers.listgenerator.gui;
+
+import com.jfoenix.controls.JFXButton;
+import com.jfoenix.controls.JFXListView;
+import com.jfoenix.controls.JFXTextArea;
+import com.jfoenix.controls.JFXTextField;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class CommandListGeneratorController implements Initializable {
+
+    @FXML
+    private JFXButton BUTTON_GENERATE;
+
+    @FXML
+    private JFXButton BUTTON_EDIT;
+
+    @FXML
+    private JFXTextArea COMMAND_OUTPUT;
+
+    @FXML
+    private JFXTextField COMMAND_INPUT;
+
+    @FXML
+    private JFXListView TAG_LIST;
+
+    @FXML
+    private JFXListView TYPE_LIST;
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+    }
+
+}
