1e49b2198850cf8d8be752158a53787f7bd33045
MrBretzel <Axelolo68@gmail.com>
2018-04-09 21:32:48 +0200

    Update

--- src/main/java/net/gunivers/listgenerator/gui/handlers/list/SyncListHandler.java
+++ src/main/java/net/gunivers/listgenerator/gui/handlers/list/SyncListHandler.java
@@ -10,20 +10,16 @@ public class SyncListHandler
     {
         this.listView1 = listView1;
         this.listView2 = listView2;
-        if (listView1.getSelectionModel().getSelectedItems().size() > 0 || listView2.getSelectionModel().getSelectedItems().size() > 0)
+
+        if (listView1.getSelectionModel().getSelectedItems().size() <= 0 || listView2.getSelectionModel().getSelectedItems().size() <= 0)
         {
             listView1.getSelectionModel().clearAndSelect(0);
             listView2.getSelectionModel().clearAndSelect(0);
         }
 
-        listView1.setOnMouseClicked(event -> {
-            System.out.println(listView1.getSelectionModel().getSelectedIndex());
-            sync(listView1.getSelectionModel().getSelectedIndex());
-        });
+        listView1.setOnMouseReleased(event -> sync(listView1.getSelectionModel().getSelectedIndex()));
 
-        listView2.setOnMouseClicked(event -> {
-            sync(listView2.getSelectionModel().getSelectedIndex());
-        });
+        listView2.setOnMouseReleased(event -> sync(listView2.getSelectionModel().getSelectedIndex()));
     }
 
     public void sync(int index)
