fa6e8f290ada671f90ce875229d64cc06847cff9
A:Z <justin.carel@free.fr>
2018-04-12 23:04:35 +0200

    Even more readable !

--- src/main/java/net/gunivers/listgenerator/util/Calculator.java
+++ src/main/java/net/gunivers/listgenerator/util/Calculator.java
@@ -1,13 +1,12 @@
 package net.gunivers.listgenerator.util;
 
 public class Calculator {
-	
+
 	private int index = 0;
 	private String text;
 	private String lexeme = "";
 	private String lexemeType = "";
-	
-	
+
 	public Calculator(String text) {
 		this.text = text;
 	}
@@ -15,45 +14,44 @@ public class Calculator {
 	public void read() {
 		lexeme = "";
 		lexemeType = "";
-		
-		while(index < text.length() && text.charAt(index) == ' ')
+
+		while (index < text.length() && text.charAt(index) == ' ')
 			index++;
-		
-		
-		if(index >= text.length())
+
+		if (index >= text.length())
 			lexemeType = "EOL";
-		
+
 		else {
-			if(Character.toString(text.charAt(index)).matches("\\d(.\\d)?")) {
+			if (Character.toString(text.charAt(index)).matches("\\d(.\\d)?")) {
 				lexemeType = "NUMBER";
-				while(index < text.length() && Character.toString(text.charAt(index)).matches("[\\d\\.]")) {
+				while (index < text.length() && Character.toString(text.charAt(index)).matches("[\\d\\.]")) {
 					lexeme += text.charAt(index);
 					index++;
 				}
-				
-			} else if(Character.toString(text.charAt(index)).matches("[\\(\\)\\-\\+\\*\\/\\%]")) {
+
+			} else if (Character.toString(text.charAt(index)).matches("[\\(\\)\\-\\+\\*\\/\\%]")) {
 				lexemeType = "SYMBOL";
 				lexeme = Character.toString(text.charAt(index));
 				index++;
-				
-			} else throw new UnsupportedOperationException("Caractère invalide");
+
+			} else
+				throw new UnsupportedOperationException("Caractère invalide");
 		}
 	}
-	
-	
+
 	public void disp() {
 		read();
-		
-		while(!lexemeType.equals("EOL")) {
+
+		while (!lexemeType.equals("EOL")) {
 			read();
 		}
 	}
 
 	public double readNumber() {
-		if(!lexemeType.equals("NUMBER")) {
+		if (!lexemeType.equals("NUMBER")) {
 			throw new UnsupportedOperationException("lexemeType should be a Number");
 		}
-		
+
 		double value = Double.parseDouble(lexeme);
 		read();
 		return value;
@@ -61,69 +59,70 @@ public class Calculator {
 
 	public double readExpression() {
 		double value = readTerm();
-		
-		while(lexemeType.equals("SYMBOL") && (lexeme.equals("+") || lexeme.equals("-"))) {
+
+		while (lexemeType.equals("SYMBOL") && (lexeme.equals("+") || lexeme.equals("-"))) {
 			String op = lexeme;
 			read();
 			double value2 = readTerm();
-			
-			if(op.equals("+"))
+
+			if (op.equals("+"))
 				value += value2;
-			
-			if(op.equals("-"))
+
+			if (op.equals("-"))
 				value -= value2;
 		}
-		
+
 		return value;
 	}
-	
+
 	public double readFactor() {
 		double value;
-		
-		if(lexemeType.equals("SYMBOL") && lexeme.equals("(")) {
+
+		if (lexemeType.equals("SYMBOL") && lexeme.equals("(")) {
 			read();
 			value = (double) readExpression();
-			
-			if(lexemeType.equals("SYMBOL") && lexeme.equals(")"))
+
+			if (lexemeType.equals("SYMBOL") && lexeme.equals(")"))
 				read();
-			
-		} else value = readNumber();
-		
+
+		} else
+			value = readNumber();
+
 		return value;
 	}
-	
+
 	public double readTerm() {
 		double value = readFactor();
-		
-		while(lexemeType.equals("SYMBOL") && (lexeme.equals("*") || lexeme.equals("/") || lexeme.equals("%"))) {
+
+		while (lexemeType.equals("SYMBOL") && (lexeme.equals("*") || lexeme.equals("/") || lexeme.equals("%"))) {
 			String op = lexeme;
 			read();
 			double value2 = readFactor();
-			
-			if(op.equals("*"))
+
+			if (op.equals("*"))
 				value *= value2;
-			
-			if(op.equals("/"))
+
+			if (op.equals("/"))
 				value /= value2;
-			
-			if(op.equals("%"))
+
+			if (op.equals("%"))
 				value %= value2;
 		}
-		
+
 		return value;
 	}
-	
+
 	public double calculate() {
 		double value;
 		read();
-			
-		if(!lexemeType.equals("EOL")) {
+
+		if (!lexemeType.equals("EOL")) {
 			value = readExpression();
-				
-			if(lexemeType.equals("EOL"))
+
+			if (lexemeType.equals("EOL"))
 				return value;
 		}
-			
+
 		return 0;
 	}
 }
