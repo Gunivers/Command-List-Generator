1c8a43f4af82b0d9ea12c24664cd48ffc9641934
theogiraudet <theo.giraudet@orange.fr>
2018-04-22 13:14:14 +0200

    Merge error

--- src/main/java/net/gunivers/listgenerator/gui/CommandListGeneratorController.java
+++ src/main/java/net/gunivers/listgenerator/gui/CommandListGeneratorController.java
@@ -15,106 +15,93 @@ import net.gunivers.listgenerator.gui.handlers.list.SyncListHandler;
 import net.gunivers.listgenerator.gui.util.OnlyIntChangeListener;
 
 import java.net.URL;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.ResourceBundle;
+import java.util.Set;
 
-public class CommandListGeneratorController implements Initializable
-{
-    public static CommandListGeneratorController CONTROLLER;
+public class CommandListGeneratorController implements Initializable {
+	public static CommandListGeneratorController CONTROLLER;
 
-    public static StackPane MAIN_PANE;
+	public static StackPane MAIN_PANE;
 
-    @FXML
-    private StackPane PANE;
+	@FXML
+	private StackPane PANE;
 
-    @FXML
-    private JFXButton BUTTON_GENERATE;
+	@FXML
+	private JFXButton BUTTON_GENERATE;
 
-    @FXML
-    private JFXButton BUTTON_EDIT;
+	@FXML
+	private JFXButton BUTTON_EDIT;
 
-    @FXML
-    private JFXTextArea COMMAND_OUTPUT;
+	@FXML
+	private JFXTextArea COMMAND_OUTPUT;
 
-    @FXML
-    private JFXTextField COMMAND_INPUT;
+	@FXML
+	private JFXTextField COMMAND_INPUT;
 
-    @FXML
-    private JFXTextField MAX_COMMAND;
+	@FXML
+	private JFXTextField MAX_COMMAND;
 
-    @FXML
-    private JFXListView<Label> TAG_LIST;
+	@FXML
+	private JFXListView<Label> TAG_LIST;
 
-    @FXML
-    private JFXListView<Label> TYPE_LIST;
+	@FXML
+	private JFXListView<Label> TYPE_LIST;
 
+	private SyncListHandler syncListHandler = null;
 
-    private SyncListHandler syncListHandler = null;
+	@Override
+	public void initialize(URL location, ResourceBundle resources) {
+		CONTROLLER = this;
+		MAIN_PANE = PANE;
 
-    @Override
-    public void initialize(URL location, ResourceBundle resources)
-    {
-        CONTROLLER = this;
-        MAIN_PANE = PANE;
+		syncListHandler = new SyncListHandler(TAG_LIST, TYPE_LIST);
 
-        syncListHandler = new SyncListHandler(TAG_LIST, TYPE_LIST);
+		MAX_COMMAND.textProperty().addListener(new OnlyIntChangeListener(MAX_COMMAND));
 
-        MAX_COMMAND.textProperty().addListener(new OnlyIntChangeListener(MAX_COMMAND));
+		COMMAND_INPUT.setOnKeyTyped(new TextFieldCommandChangeHandler(COMMAND_INPUT));
 
-        TextFieldCommandChangeHandler handler = new TextFieldCommandChangeHandler(COMMAND_INPUT);
+		BUTTON_GENERATE
+				.setOnAction(new ButtonGenerateHandler(BUTTON_GENERATE, COMMAND_INPUT, COMMAND_OUTPUT, getMaxSize()));
 
-        COMMAND_INPUT.setOnKeyTyped(handler);
-        COMMAND_INPUT.setOnKeyReleased(handler);
+		BUTTON_EDIT.setOnAction(new ButtonEditHandler());
+	}
 
-        BUTTON_GENERATE.setOnAction(new ButtonGenerateHandler(BUTTON_GENERATE, COMMAND_INPUT, COMMAND_OUTPUT, getMaxSize()));
+	public SyncListHandler getSyncListHandler() {
+		return syncListHandler;
+	}
 
-        BUTTON_EDIT.setOnAction(new ButtonEditHandler());
-    }
+	public void checksTag(Set<String> tags) {
+		ArrayList<String> displayed = new ArrayList<>();
+		for (Label l : TAG_LIST.getItems())
+			displayed.add(l.getText());
 
-    public SyncListHandler getSyncListHandler()
-    {
-        return syncListHandler;
-    }
+		ArrayList<Label> needRemoveTag = new ArrayList<>();
 
-    public void checksTag(Set<String> tags)
-    {
-        List<String> listASupprimé = new ArrayList<>();
-        for (Label l : TAG_LIST.getItems())
-            listASupprimé.add(l.getText());
+		ArrayList<Label> needAddedTag = new ArrayList<>();
 
-        List<String> list = new ArrayList<>();
-        for (String s : listASupprimé)
-            list.add(s);
+		for (String tag : tags) {
+			if (!displayed.contains(tag)) {
+				needAddedTag.add(new Label(tag));
+			}
+		}
 
-        listASupprimé.removeAll(tags);
+		for (Label label : TAG_LIST.getItems()) {
+			if (!tags.contains(label.getText()))
+				needRemoveTag.add(label);
 
-        tags.removeAll(list);
+		}
 
-        if (!listASupprimé.isEmpty() || !tags.isEmpty())
-        {
-            ArrayList<Label> bgj = new ArrayList<>();
-            for (Label s : TAG_LIST.getItems())
-                bgj.add(s);
+		TAG_LIST.getItems().removeAll(needRemoveTag);
 
-            if (bgj.size() > 0)
-                for (Label label : bgj)
-                    for (String str : listASupprimé)
-                        if (label.getText().equalsIgnoreCase(str))
-                            TAG_LIST.getItems().remove(label);
+		TAG_LIST.getItems().addAll(needAddedTag);
+	}
 
-
-            for (String str : tags)
-                TAG_LIST.getItems().add(new Label(str));
-        }
-    }
-
-    public int getMaxSize()
-    {
-        try
-        {
-            return Integer.valueOf(MAX_COMMAND.getText());
-        } catch (Exception e)
-        {
-            return 0;
-        }
-    }
+	public int getMaxSize() {
+		try {
+			return Integer.valueOf(MAX_COMMAND.getText());
+		} catch (Exception e) {
+			return 0;
+		}
+	}
 }
