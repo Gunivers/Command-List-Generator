beed076ef5c6be44df2ddb076ef60f8c9147fa08
MrBretzel <Axelolo68@gmail.com>
2018-04-22 14:45:48 +0200

    Added ScoreInterpolation

---
+++ src/main/java/net/gunivers/listgenerator/gui/functionality/ScoreInterpolationController.java
@@ -0,0 +1,47 @@
+package net.gunivers.listgenerator.gui.functionality;
+
+import com.jfoenix.controls.JFXCheckBox;
+import com.jfoenix.controls.JFXTextField;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+import net.gunivers.listgenerator.gui.CommandListGeneratorController;
+import net.gunivers.listgenerator.gui.handlers.ButtonNextHandler;
+import net.gunivers.listgenerator.gui.handlers.list.SyncListHandler;
+import net.gunivers.listgenerator.gui.util.FunctionalityController;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class ScoreInterpolationController extends FunctionalityController implements Initializable
+{
+    @FXML
+    private JFXCheckBox CHECK_BOX;
+
+    @FXML
+    private JFXTextField TEXT_FIELD_1;
+
+    @FXML
+    private JFXTextField TEXT_FIELD_2;
+
+    @FXML
+    private JFXTextField TEXT_FIELD_3;
+
+    @FXML
+    private JFXTextField TEXT_FIELD_4;
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources)
+    {
+        getDoneButton().setOnAction(event -> saveAll());
+    }
+
+    @Override
+    public void saveAll()
+    {
+        ButtonNextHandler.newDialog.close();
+        int index = CommandListGeneratorController.SYNC_LIST_HANDLER.getListViewOne().getSelectionModel().getSelectedIndex();
+        Label label = new Label("ScoreInterpolation");
+        CommandListGeneratorController.SYNC_LIST_HANDLER.putInAndSelect(SyncListHandler.ListNumber.TWO, label, index);
+    }
+}
