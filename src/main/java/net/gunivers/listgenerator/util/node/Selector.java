e8245f15761a69b7d9dc4242142c6229805e7fea
A:Z <justin.carel@free.fr>
2018-05-05 18:33:19 +0200

    Add SelectorFields.java

--- src/main/java/net/gunivers/listgenerator/util/node/Selector.java
+++ src/main/java/net/gunivers/listgenerator/util/node/Selector.java
@@ -8,8 +8,16 @@ public class Selector extends Node {
 	
 	@Override
 	public boolean matches(String selector) {
+		selector = selector.substring(2, selector.length() - 2);
 		
+		String[] fields = selector.split(",");
 		
+		for (String field : fields) {
+			SelectorFields value = Enum.valueOf(SelectorFields.class, field.split("=")[0]);
+			if (value == null) return false;
+			
+			Class clazz = value.getParameter();
+		}
 		
 		return false;
 	}
---
+++ src/main/java/net/gunivers/listgenerator/util/node/SelectorFields.java
@@ -0,0 +1,46 @@
+package net.gunivers.listgenerator.util.node;
+
+@SuppressWarnings("rawtypes")
+public enum SelectorFields {
+	ADVANCEMENTS(AdvancementParser.class, true),
+	DISTANCE(Double.class, true),
+	DX(Double.class, true),
+	DY(Double.class, true),
+	DZ(Double.class, true),
+	GAMEMODE(Integer.class),
+	LEVEL(Integer.class),
+	LIMIT(Integer.class, true),
+	NAME(String.class),
+	NBT(NbtParser.class, true),
+	SCORES(ScoreParser.class, true),
+	SORT(String.class, true),
+	TAG(String.class),
+	TEAM(String.class),
+	TYPE(Entity.class),
+	X(Double.class),
+	X_ROTATION(Double.class),
+	Y(Double.class),
+	Y_ROTATION(Double.class),
+	Z(Double.class);
+	
+	
+	Class clazz;
+	boolean singleton = false;
+	
+	private SelectorFields(Class parameter) {
+		this.clazz = parameter;
+	}
+	
+	private SelectorFields(Class parameter, boolean singleton) {
+		this.clazz = parameter;
+		this.singleton = singleton;
+	}
+	
+	public String toString() {
+		return this.name().toLowerCase();
+	}
+	
+	public Class getParameter() {
+		return this.clazz;
+	};
+}
