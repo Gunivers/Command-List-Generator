ff06a2d2a5541b80ccd386ab36610b3083666982
theo <theo.giraudet@orange.fr>
2018-05-08 21:08:24 +0200

    Node fixe fixe

--- src/main/java/net/gunivers/listgenerator/util/node/Node.java
+++ src/main/java/net/gunivers/listgenerator/util/node/Node.java
@@ -4,35 +4,37 @@ import java.util.ArrayList;
 import java.util.Arrays;
 
 public class Node {
-	
+
 	protected ArrayList<Node> children = new ArrayList<Node>();
 	protected String tag = "";
-	
-	
-	public Node(String tag, Node ... children) {
+
+	public Node(String tag, Node... children) {
 		this.tag = tag;
 		setChildren(children);
 	}
-	
-	public void setChildren(Node ... children) {
+
+	public void setChildren(Node... children) {
 		this.children.addAll(Arrays.asList(children));
 	}
-	
+
 	public boolean addChild(Node child) {
 		return children.add(child);
 	}
-	
+
 	public boolean removeChild(Node child) {
 		return children.remove(child);
 	}
-	
+
 	public ArrayList<Node> getChildren() {
 		return children;
 	}
-	
+
 	public String getTag() {
 		return tag;
 	}
-	
-	
+
+	public boolean matches(String tag) {
+		return this.tag == tag;
+	}
+
 }
