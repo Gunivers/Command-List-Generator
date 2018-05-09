79daa186f036bfcbc0ef35d637b83e3cccd0157c
theo <theo.giraudet@orange.fr>
2018-05-09 15:30:03 +0200

    Fixe noeud silencieux

--- src/main/java/net/gunivers/listgenerator/util/node/Command.java
+++ src/main/java/net/gunivers/listgenerator/util/node/Command.java
@@ -30,14 +30,14 @@ public abstract class Command extends CommandNode {
 	}
 	
 	private int browseAndCompare(String[] cmd, CommandNode node) {
+		int range = node.isSilent() ? 0 : 1;
 		if(cmd.length == 1 && node.matches(cmd[0])) return -1;
 		else if(cmd.length == 1) return 0;
-		else if(!node.matches(cmd[0])) return 0;
-		int range = node.isSilent() ? 0 : 1;
+		else if(range > 0 && !node.matches(cmd[0])) return 0;
 		ArrayList<Integer> list = new ArrayList<Integer>();
 		for(Node child : node.getChildren()) {
 			int i = browseAndCompare(Arrays.copyOfRange(cmd, range, cmd.length), (CommandNode)child);
-			list.add((i == -1) ? -1 : i + 1);
+			list.add((i == -1) ? -1 : (range > 0 ? i + 1 : i));
 		}
 		return (list.contains(-1)) ? -1 : Collections.max(list);
 	}
