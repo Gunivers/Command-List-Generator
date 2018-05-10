dbf70b969985d51865e1f553402d1f601fbfef83
A:Z <justin.carel@free.fr>
2018-05-10 13:19:25 +0200

    Update TODO

--- src/main/java/net/gunivers/commandparser/selector/SelectorFields.java
+++ src/main/java/net/gunivers/commandparser/selector/SelectorFields.java
@@ -2,35 +2,35 @@ package net.gunivers.commandparser.selector;
 
 public enum SelectorFields {
 	//ADVANCEMENTS(AdvancementParser.class, true),
-	DISTANCE("[0-9]+(\\.[0-9]+)?", true),
-	DX("[0-9]+(\\.[0-9]+)?", true),
-	DY("[0-9]+(\\.[0-9]+)?", true),
-	DZ("[0-9]+(\\.[0-9]+)?", true),
-	GAMEMODE(""),
-	LEVEL("[0-9]+"),
-	LIMIT("[0-9]+", true),
-	NAME("\\w"),
+	DISTANCE(((String s) -> true), true),
+	DX(((String s) -> true), true),
+	DY(((String s) -> true), true),
+	DZ(((String s) -> true), true),
+	GAMEMODE(((String s) -> true)),
+	LEVEL(((String s) -> true)),
+	LIMIT(((String s) -> true), true),
+	NAME(((String s) -> true)),
 	//NBT(NbtParser.class, true),
 	//SCORES(ScoreParser.class, true),
-	SORT("\\w", true),
-	TAG("\\w"),
-	TEAM("\\w"),
+	SORT(((String s) -> true), true),
+	TAG(((String s) -> true)),
+	TEAM(((String s) -> true)),
 	//TYPE(Entity.class),
-	X("[0-9]+(\\.[0-9]+)?", true),
-	X_ROTATION("[0-9]+(\\.[0-9]+)?", true),
-	Y("[0-9]+(\\.[0-9]+)?", true),
-	Y_ROTATION("[0-9]+(\\.[0-9]+)?", true),
-	Z("[0-9]+(\\.[0-9]+)?", true);
+	X(((String s) -> true), true),
+	X_ROTATION(((String s) -> true), true),
+	Y(((String s) -> true), true),
+	Y_ROTATION(((String s) -> true), true),
+	Z(((String s) -> true), true);
 	
 	
-	String match;
+	SelectorMatcher match;
 	boolean singleton = false;
 	
-	private SelectorFields(String match) {
+	private SelectorFields(SelectorMatcher match) {
 		this.match = match;
 	}
 	
-	private SelectorFields(String match, boolean singleton) {
+	private SelectorFields(SelectorMatcher match, boolean singleton) {
 		this.match = match;
 		this.singleton = singleton;
 	}
@@ -39,10 +39,5 @@ public enum SelectorFields {
 		return this.name().toLowerCase();
 	}
 	
-	public String getParameter() {
-		return this.match;
-	};
-	
-	//TODO
-	public boolean matches(String value) {return true;}
+	public boolean matches(String value) {return this.match.matches(value);}
 }
