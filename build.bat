8647208e8d12f92123b56d5a3d7c33396e4e8ade
MrBretzel <axelolo68@gmail.com>
2018-06-14 19:08:54 +0200

    Fixing build script

--- build.bat
+++ build.bat
@@ -1,15 +1,8 @@
 title Minecraft GDK Build Script
-
 cls
-
 @ECHO OFF
-
 ECHO ##########################################################################
-
 ECHO 		Minecraft GDK Build Script
-
 ECHO ##########################################################################
 
-
-gradlew --refresh-dependencies && gradlew build
- && gradlew clean
+gradlew --refresh-dependencies && gradlew build && gradlew clean
